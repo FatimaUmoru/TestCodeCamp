aa5853d0bbe29ebbd8294d57f1aaa956ecbd49ca
fatimaumoru <fatimaumoru123@gmail.com>
2023-04-25 12:00:12 +0100

    added my first java project folder task 14

---
+++ FirstJavaProject/src/Main.java
@@ -0,0 +1,12 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Fatima welcome!");
+        System.out.println("This is your first Intellij IDEA lesson!");
+        System.out.println("Ensure to do further research in addition to this for good grounding!");
+        System.out.println("Goodluck!");
+
+
+        }
+    }
